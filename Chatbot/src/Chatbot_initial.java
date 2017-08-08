33fa7ad4a7a84ecb87dde61c7eeec2d0dbeae432
garfield041 <mitchan_94@hotmail.com>
2017-08-07 22:22:08 -0700

    Added removePunctuation method

--- Chatbot/src/Chatbot_initial.java
+++ Chatbot/src/Chatbot_initial.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Chatbot_initial {
 
     static String[] responseBank = {
+        "My name is Andrea, thanks for asking!",
         "Are you sure that is what you meant?",
         "Sorry...but you're not making sense...",
         "Same, buddy. I feel the same way.",
@@ -10,21 +11,36 @@ public class Chatbot_initial {
         "Really wanting a Big Mac right now"
     };
 
+    static String removePunctuation(String input) {
+        char[] output = new char[input.length()];
+        int i = 0;
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetterOrDigit(ch) || Character.isWhitespace(ch)) {
+                output[i++] = ch;
+            }
+        }
+        return new String(output, 0, i);
+    }
+
     public static void main(String[] args) {
         boolean status = true;
         System.out.println("Hi Stranger! I'm Andrea. What's your name?");
         Scanner scan = new Scanner(System.in);
         String userName = scan.nextLine();
-        System.out.println("Hey " + userName + "!");
+        System.out.println("Hey, " + userName + "!");
         while (status) {
             Random randNumGenerator = new Random();
             System.out.println("What's your question? Say BYE to leave.");
             String questionInput = scan.nextLine();
+            questionInput = removePunctuation(questionInput);
             int randResponseNum = randNumGenerator.nextInt(responseBank.length);
             String answer = responseBank[randResponseNum];
             if (questionInput.toUpperCase().equals("BYE")) {
                 System.out.println("Please drop by again!");
                 break;
+            }
+            if (questionInput.length() == 0) {
+                System.out.println("Sorry, I don't understand your query");
             }
             else {
                 System.out.println(answer);
